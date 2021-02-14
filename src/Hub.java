fbe5520f40eb4fa27fa49482314fe7b24c22bca4
Hp <rishabh.yadav_cs18@gla.ac.in>
2021-02-14 12:21:06 +0530

    if else Ladder

---
+++ src/Hub.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Hub
+{
+
+        public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            int a = 5;
+            if (a > 2) {
+                System.out.println("hello Rishabh");
+            } else if (a < 4) {
+                System.out.println("hello sunny");
+
+            } else if (a == 5) {
+                System.out.println("hello");
+
+            } else if (a >= 6) {
+                System.out.println("kya hua");
+            }
+        }
+    
+}
